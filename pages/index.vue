4d15e6380164165da7016b4391217f89ce2a8927
adrian-borovnik <adrian.borovnik@gmail.com>
2023-06-06 22:46:20 +0200

    Changing the sprite of a pokemon

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,9 @@
   <p>You have {{ store.pokemonNum }} pokemon</p>
   <v-sheet class="d-flex justify-center">
     <v-card class="d-flex">
-      <img
-        :src="pokemon.sprites.other.dream_world.front_default"
-        alt="Image of a pokemon"
-      />
+      <img :src="pokemon.sprites.front_default" alt="Image of a pokemon" />
       <v-sheet>
         <p>{{ pokemon.name }}</p>
-        <p>{{}}</p>
       </v-sheet>
     </v-card>
   </v-sheet>
@@ -20,8 +16,7 @@
 
   const store = useContextStore()
 
-  const getData = async () => {}
   const id = Math.floor(Math.random() * 1000)
   const uri = `https://pokeapi.co/api/v2/pokemon/${id}`
-  const { data: pokemon }: any = await useFetch(uri)
+  let { data: pokemon }: any = await useFetch(uri)
 </script>
