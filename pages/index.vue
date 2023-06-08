129428aaefa5f764eb5c7319c3c16dafad49e895
adr1an456 <adrian.borovnik@gmail.com>
2023-06-08 18:36:48 +0200

    Displaying pokemon stats

--- pages/index.vue
+++ pages/index.vue
@@ -10,22 +10,45 @@
     <v-sheet width="100%" max-width="600">
       <v-card class="d-flex pa-4" v-if="!fetchError">
         <v-img
-          :src="pokemon.sprites.front_default"
+          :src="pokemon.img"
           alt="Image of a pokemon"
           aspect-ratio="1"
           max-width="200"
         />
         <v-divider vertical class="mx-4"></v-divider>
         <v-sheet>
-          <p>
+          <span>
             {{
               pokemon.name.toUpperCase()[0] +
               pokemon.name.replace('-', ' ').substring(1)
             }}
-          </p>
-          <p>
-            {{ pokemon.name }}
-          </p>
+          </span>
+          <!-- <v-list disabled>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon icon="mdi-heart" color="green"> </v-icon>
+              </template>
+              <v-list-item-title
+                >Health points: {{ pokemon.stats.hp }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list> -->
+          <div class="d-flex mt-2">
+            <v-icon icon="mdi-heart" color="green mr-2"> </v-icon>
+            <p>Health points: {{ pokemon.stats.hp }}</p>
+          </div>
+          <div class="d-flex mt-2">
+            <v-icon icon="mdi-sword" color="red mr-2"> </v-icon>
+            <p>Attack: {{ pokemon.stats.attack }}</p>
+          </div>
+          <div class="d-flex mt-2">
+            <v-icon icon="mdi-shield" color="blue mr-2"> </v-icon>
+            <p>Defence: {{ pokemon.stats.defense }}</p>
+          </div>
+          <div class="d-flex mt-2">
+            <v-icon icon="mdi-fast-forward" color="purple mr-2"> </v-icon>
+            <p>Speed: {{ pokemon.stats.speed }}</p>
+          </div>
         </v-sheet>
       </v-card>
       <v-sheet class="d-flex justify-end pt-4">
@@ -51,7 +74,7 @@
   const fetchError = ref(false)
   const loading = ref(false)
 
-  const pokemon = ref({} as PokemonResponse)
+  const pokemon = ref({} as PokemonModel)
 
   const fetchData = async () => {
     loading.value = true
@@ -59,19 +82,26 @@
       const id = Math.floor(Math.random() * 1000)
       const uri = `https://pokeapi.co/api/v2/pokemon/${id}`
       const res = await fetch(uri)
-      const rawData = await res.json()
+      const rawData: PokemonResponse = await res.json()
 
       loading.value = false
       fetchError.value = false
 
-      const pokemonData: PokemonResponse = {
+      console.log(rawData)
+
+      const pokemonData: PokemonModel = {
+        id: Math.floor(Math.random() * 999999999),
         name: rawData.name,
-        stats: rawData.stats,
-        abilities: rawData.abilities,
-        sprites: {
-          front_default: rawData.sprites.front_default,
+        stats: {
+          hp: rawData.stats[0]?.base_stat,
+          attack: rawData.stats[1]?.base_stat,
+          defense: rawData.stats[2]?.base_stat,
+          speed: rawData.stats[5]?.base_stat,
         },
+        mainAbility: rawData.abilities[0]?.ability.name,
+        img: rawData.sprites.front_default,
       }
+
       pokemon.value = pokemonData
       console.log(pokemonData)
     } catch (e) {
@@ -100,20 +130,20 @@
   await fetchData()
 
   const handleKeep = async () => {
-    const tempPokemon: PokemonModel = {
-      id: Math.floor(Math.random() * 999999999), // idealy would be using uuid
-      name: pokemon.value.name,
-      stats: {
-        hp: pokemon.value.stats[0].base_stat,
-        attack: pokemon.value.stats[1].base_stat,
-        defense: pokemon.value.stats[2].base_stat,
-        speed: pokemon.value.stats[3].base_stat,
-      },
-      mainAbility: pokemon.value.abilities[0].ability.name,
-      img: pokemon.value.sprites.front_default,
-    }
+    // const tempPokemon: PokemonModel = {
+    //   id: Math.floor(Math.random() * 999999999), // idealy would be using uuid
+    //   name: pokemon.value.name,
+    //   stats: {
+    //     hp: pokemon.value.stats[0].base_stat,
+    //     attack: pokemon.value.stats[1].base_stat,
+    //     defense: pokemon.value.stats[2].base_stat,
+    //     speed: pokemon.value.stats[3].base_stat,
+    //   },
+    //   mainAbility: pokemon.value.abilities[0].ability.name,
+    //   img: pokemon.value.sprites.front_default,
+    // }
 
-    store.addPokemon(tempPokemon)
+    if (pokemon.value) store.addPokemon(pokemon.value)
     await fetchData()
   }
 </script>
