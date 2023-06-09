3eef0b0337f818576127ae882c014bb2efa936a6
adr1an456 <adrian.borovnik@gmail.com>
2023-06-09 09:30:05 +0200

    Error handling

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-sheet class="d-flex flex-column align-center justify-center" height="50%">
+    <h1>{{ error.statusCode }}</h1>
+    <h2>Ooo oh...</h2>
+    <p>There was an error...</p>
+    <code>{{ error.message }}</code>
+
+    <v-btn @click="handleError">Go back</v-btn>
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+  defineProps(['error'])
+
+  const handleError = () => clearError({ redirect: '/' })
+</script>
